ac5b54b795d975c3a3644ffa1df3e9f1489a1c38
Robbie Hickey <robbie.hickey@msionline.com>
2016-09-23 10:57:39 -0500

    Initial impl for chatty version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MomentModule } from 'angular2-moment';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+
+import { HackerNewsAPIService } from './hackernews-api.service';
+import { DomainPipe } from './domain.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { HackerNewsAPIService } from '../hackernews-api.service';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+
+export class StoriesComponent implements OnInit {
+  items;
+
+  constructor(private _hackerNewsAPIService: HackerNewsAPIService) {}
+
+  ngOnInit() {
+    this._hackerNewsAPIService.fetchStories()
+      .subscribe(
+        items => this.items = items,
+        error => console.log('Error fetching stories'));
+  }
+}
